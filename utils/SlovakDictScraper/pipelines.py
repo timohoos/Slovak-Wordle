4086c583fe6ca179675b4eb2115908a16531ca03
xhoos <xhoos@fi.muni.cz>
2022-07-31 10:17:56 +0200

    Use Scrapy pipeline to process scraped words

--- utils/SlovakDictScraper/pipelines.py
+++ utils/SlovakDictScraper/pipelines.py
@@ -5,7 +5,20 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import re
 
-class SlovakdictscraperPipeline(object):
+
+class SlovakDictScraperPipeline(object):
+
+    def open_spider(self, spider):
+        self.file = open('slovakdict.txt', 'a')
+
+    def close_spider(self, spider):
+        self.file.close()
+
     def process_item(self, item, spider):
+        for idx in item:
+            if re.search("^[^-().’! ]*$", item[idx]) and len(item[idx]) == 5:
+                self.file.write(f"{item[idx]}\n")
+
         return item
--- utils/SlovakDictScraper/spiders/slovakdict_spider.py
+++ utils/SlovakDictScraper/spiders/slovakdict_spider.py
@@ -1,5 +1,4 @@
 import scrapy
-import re
 
 
 class SlovakdictSpider(scrapy.Spider):
@@ -9,19 +8,16 @@ class SlovakdictSpider(scrapy.Spider):
         yield scrapy.Request(url='https://slovnik.aktuality.sk/pravopis/vsetky/a/1/', callback=self.parse)
 
     def parse(self, response):
-        filename = 'slovakdict.txt'
-        words = list(filter(lambda word: re.search("^[^-().’! ]*$", word) and len(word) == 5,
-                     response.css('#reg-zoznam li > a::text').getall()))
-
-        fd = open(filename, "a", encoding="utf-8")
-        for word in words:
-            fd.write(f"{word}\n")
-        fd.close()
-
         next_page_url = response.css("#nav a.sipkar::attr(href)").get()
+
         if next_page_url is not None:
             yield scrapy.Request(response.urljoin(next_page_url))
         else:
             next_letter_url = response.css("a.vybrane + *::attr(href)").get()
             yield scrapy.Request(response.urljoin(next_letter_url))
 
+        words = dict()
+        for idx, word in enumerate(response.css('#reg-zoznam li > a::text').getall()):
+            words[idx] = word
+
+        yield words
