e42acedd966dfce86106889be420ea9199a2d137
xhoos <xhoos@fi.muni.cz>
2022-08-09 21:38:30 +0200

    Refactor game.py

--- backend/game.py
+++ backend/game.py
@@ -1,53 +1,20 @@
 from collections import Counter
+from enum import Enum
+
+
+class State(str, Enum):
+    NOT_USED = "not used"
+    NOT_PRESENT = "not present"
+    WRONG_PLACEMENT = "wrong placement"
+    CORRECT_PLACEMENT = "correct placement"
 
 
 class Game:
     def __init__(self, word):
         self.word = word
         self.correct = False
-        self.overall_state = {
-            "a": "not used",
-            "b": "not used",
-            "c": "not used",
-            "d": "not used",
-            "e": "not used",
-            "f": "not used",
-            "g": "not used",
-            "h": "not used",
-            "i": "not used",
-            "j": "not used",
-            "k": "not used",
-            "l": "not used",
-            "m": "not used",
-            "n": "not used",
-            "o": "not used",
-            "p": "not used",
-            "q": "not used",
-            "r": "not used",
-            "s": "not used",
-            "t": "not used",
-            "u": "not used",
-            "v": "not used",
-            "w": "not used",
-            "x": "not used",
-            "y": "not used",
-            "z": "not used",
-            "á": "not used",
-            "ä": "not used",
-            "é": "not used",
-            "í": "not used",
-            "ó": "not used",
-            "ô": "not used",
-            "ú": "not used",
-            "ý": "not used",
-            "č": "not used",
-            "ď": "not used",
-            "ĺ": "not used",
-            "ľ": "not used",
-            "ň": "not used",
-            "ť": "not used",
-            "ž": "not used"
-        }
+        self.overall_state = {letter: State.NOT_USED for letter in
+                              "abcdefghijklmnopqrstuvwxyzáäéíóôúýčďĺľňťž"}
         self.guesses_state = []
 
     def check_word(self, guess, letter_count, guess_state):
@@ -57,8 +24,8 @@ class Game:
             if self.word[i] == guess[i]:
                 letter = self.word[i]
                 letter_count[letter] -= 1
-                self.overall_state[letter] = "correct placement"
-                guess_state[i] = "correct placement"
+                self.overall_state[letter] = State.CORRECT_PLACEMENT
+                guess_state[i] = State.CORRECT_PLACEMENT
             else:
                 correct = False
 
@@ -66,19 +33,19 @@ class Game:
 
     def check_letters(self, guess, letter_count, guess_state):
         for i, letter in enumerate(guess):
-            if guess_state[i] == "correct placement":
+            if guess_state[i] == State.CORRECT_PLACEMENT:
                 continue
             if letter in letter_count:
                 if letter_count[letter] > 0:
                     letter_count[letter] -= 1
-                    if self.overall_state != "correct placement":
-                        self.overall_state[letter] = "wrong_placement"
-                    guess_state[i] = "wrong placement"
+                    if self.overall_state != State.CORRECT_PLACEMENT:
+                        self.overall_state[letter] = State.WRONG_PLACEMENT
+                    guess_state[i] = State.WRONG_PLACEMENT
             else:
-                self.overall_state[letter] = "not present"
+                self.overall_state[letter] = State.NOT_PRESENT
 
     def add_guess(self, guess):
-        guess_state = ["not present" for _ in range(len(guess))]
+        guess_state = [State.NOT_PRESENT for _ in range(len(guess))]
         letter_count = Counter(self.word)
         self.correct = self.check_word(guess, letter_count, guess_state)
         self.check_letters(guess, letter_count, guess_state)
